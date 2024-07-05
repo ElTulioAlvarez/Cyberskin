1b27eee54935883f433286d04daa2886b809c131
ElTulioAlvarez <79723403+ElTulioAlvarez@users.noreply.github.com>
2024-07-05 16:31:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+
+
+  ScrollReveal().reveal(".cards-banner-one", {
+      duration: 1000,
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out"
+    });
+  
+    ScrollReveal().reveal(".cards-banner-two", {
+      duration: 1000,
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out"
+    });
+
+  
+    ScrollReveal().reveal(".news-cards", {
+      duration: 1000,
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out"
+    });
+  
+    ScrollReveal().reveal(".social", {
+      duration: 1000,
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out"
+    });
+  
+    ScrollReveal().reveal(".footer-links", {
+      duration: 1000,
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out"
+    });
+  
+    ScrollReveal().reveal(".footer", {
+      duration: 1000,
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out"
+    });
